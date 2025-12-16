a3b21e4f207f8cf8fb36e9d1cdd2a68d087cf1ce
AteszXD <keria@kkszki.hu>
2025-12-16 20:33:40 +0100

    menü alapja beépítvve

--- Mitulatbati/Program.cs
+++ Mitulatbati/Program.cs
@@ -13,58 +13,95 @@ namespace Mitulatbati
             Directory.SetCurrentDirectory(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\"))); // Ez azért van, hogy a html fájlt a .css és .js mellé mentse
             Console.Title = "Szélesbálási Fedettpályás Kalaplengető Verseny";
 
-            Console.WriteLine("*** MENÜ ***");
-            Console.WriteLine("1. Eredményfelvétel");
-            Console.WriteLine("2. Rekordmódosítás");
-            Console.WriteLine("3. Eredménytörlés");
-            Console.WriteLine("4. Kilépés");
-
-            Console.Write("Kérem válasszon menüpontot: ");
-            int input = int.Parse(Console.ReadLine());
-
-            while (input < 1 || input > 4)
+            int currentPoint = 1;
+            bool selected = false;
+            do
             {
-                Console.WriteLine("Érvénytelen menüpont!");
-                Console.Write("Kérem válasszon menüpontot: ");
-                input = int.Parse(Console.ReadLine());
-            }
+                DisplayMenu(currentPoint);
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Enter:
+                        selected = true;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (currentPoint > 1)
+                        {
+                            currentPoint -= 1;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentPoint < 4)
+                                currentPoint += 1;
+                        }
+                        break;
+                }
+            } while (!selected);
 
             List<SQLManager> versenyzok = new SQLManager().ReadUserDatabase();
 
-            if (input == 1)
+            switch (currentPoint)
             {
-                new SQLManager().InsertIntoDatabase();
-            }
-            else if (input == 2)
-            {
-                Console.Clear();
-                foreach (SQLManager versenyzo in versenyzok)
-                {
-                    Console.WriteLine($"{versenyzo.Id}, {versenyzo.Name}");
-                }
-                Console.Write("Kérem adja meg a módosítandó versenyző sorszámát: ");
-                int id = int.Parse(Console.ReadLine());
-                new SQLManager().UpdateDatabase(id);
-            }
-            else if (input == 3)
-            {
-                Console.Clear();
-                foreach (SQLManager versenyzo in versenyzok)
-                {
-                    Console.WriteLine($"{versenyzo.Id}, {versenyzo.Name}");
-                }
-                Console.Write("Kérem adja meg a törlendő versenyző sorszámát: ");
-                int id = int.Parse(Console.ReadLine());
-                new SQLManager().DeleteFromDatabase(id);
-            }
-            else if (input == 4)
-            {
-                Environment.Exit(0);
+                case 1:
+                    new SQLManager().InsertIntoDatabase();
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    foreach (SQLManager versenyzo in versenyzok)
+                    {
+                        Console.WriteLine($"{versenyzo.Id}, {versenyzo.Name}");
+                    }
+                    Console.Write("Kérem adja meg a módosítandó versenyző sorszámát: ");
+                    int updateId = int.Parse(Console.ReadLine());
+                    new SQLManager().UpdateDatabase(updateId);
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    foreach (SQLManager versenyzo in versenyzok)
+                    {
+                        Console.WriteLine($"{versenyzo.Id}, {versenyzo.Name}");
+                    }
+                    Console.Write("Kérem adja meg a törlendő versenyző sorszámát: ");
+                    int deleteId = int.Parse(Console.ReadLine());
+                    new SQLManager().DeleteFromDatabase(deleteId);
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    Console.Beep();
+                    Console.Write("Biztosan kilép? (i/n): ");
+                    if (Console.ReadKey().Key != ConsoleKey.I)
+                    {
+                        currentPoint = 1;
+                    }
+                    break;
             }
 
             versenyzok = new SQLManager().ReadUserDatabase();
             versenyzok = new HTMLManager().OrderCompetitors(versenyzok);
             new HTMLManager().GenerateHTML(versenyzok);
+        }
+
+        static void DisplayMenu(int selectedOption)
+        {
+            Console.Clear();
+            string[] menuItems = { "1. Eredményfelvétel", "2. Rekordmódosítás", "3. Eredménytörlés", "4. Kilépés" };
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                if (i + 1 == selectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+                Console.WriteLine(menuItems[i]);
+            }
+            Console.ResetColor();
         }
     }
 }
